f1b7bb04965bcf29e9b714888c9579ff4b154a52
Gp32Prog66 <szojo7@hotmail.com>
2024-10-03 13:49:07 -0400

    Added WebScraping Source Code From Assignment 10

---
+++ WebScraping.py
@@ -0,0 +1,22 @@
+#Will need to grab the request library
+from bs4 import BeautifulSoup
+import requests
+
+#GET Request
+req = requests.get('https://localhost:5500')
+soup = BeautifulSoup(req.text, "html.parser")
+
+
+office_supplies = soup.findAll("span", attrs={"class":"text"})
+
+
+#For Loop To Gather the Quote and the Author of the Quote
+for quote in office_supplies:
+    print(office_supplies.text)
+
+#Input
+continueProgram = input("Pause Break. Type anything to see the rest of the results in html ")
+
+#Gathers HTML Code
+print(req)
+print(req.content)
