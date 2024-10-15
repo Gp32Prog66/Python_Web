f0a482a3b395cc3b1a0d6752f3c5a95980d24352
Gp32Prog66 <szojo7@hotmail.com>
2024-10-15 11:33:32 -0700

    Connected to the Database

--- postGresJSONScript.py
+++ postGresJSONScript.py
@@ -1,16 +1,29 @@
 import psycopg2
 
 db = psycopg2.connect(database="office_supplies",
-                      host = "LocalHost",
-                      user = "Postgres",
+                      host = "localHost",
+                      user = "postgres",
                       password="GAmeP88raMD####",
                       port="5432")
 
 
-cursorCon = conn.cursor()
+cursorCon = db.cursor()
 
 cursorCon.execute("SELECT * FROM supplies")
 
 #Gather Everything from the Database
+
+for row in cursorCon.fetchall():
+    print(row)
+
+
+#Use Fetchall Statement
+print('Using Fetchall Statement')
 print(cursorCon.fetchall())
 
+db.commit()
+
+
+#Close Connection and Database
+db.close()
+cursorCon.close()
