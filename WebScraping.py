20f6d651124c3668f7975c04fcebae6f87899469
Gp32Prog66 <szojo7@hotmail.com>
2024-10-16 22:31:37 -0400

    Added Comments

--- WebScraping.py
+++ WebScraping.py
@@ -3,14 +3,17 @@ from bs4 import BeautifulSoup
 import requests
 
 #GET Request
+
+#Scrape From Render Website
 req = requests.get('https://python-web-7bsx.onrender.com')
 soup = BeautifulSoup(req.text, "html.parser")
 
 
+#Scraping HTML 
 office_supplies = soup.findAll("span", attrs={"class":"text"})
 
 
-#For Loop To Gather the Quote and the Author of the Quote
+#For Loop To Gather the Office Supplies From the HTML Page
 for quote in office_supplies:
     print(office_supplies)
 
--- postGresJSONScript.py
+++ postGresJSONScript.py
@@ -1,18 +1,21 @@
 import psycopg2
 
+#Connect to PostGres
 db = psycopg2.connect(database="office_supplies",
                       host = "localHost",
                       user = "postgres",
                       password="GAmeP88raMD####",
                       port="5432")
 
-
+#Opening Cursor in a Database
 cursorCon = db.cursor()
 
+#Execute Simple Query
 cursorCon.execute("SELECT * FROM supplies")
 
 #Gather Everything from the Database
 
+#Print out Office Supplies in Rows
 for row in cursorCon.fetchall():
     print(row)
 
@@ -21,6 +24,7 @@ for row in cursorCon.fetchall():
 print('Using Fetchall Statement')
 print(cursorCon.fetchall())
 
+#Save Data
 db.commit()
 
 
